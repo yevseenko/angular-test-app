5b35abaed2cccd4aa5a6ac26928d00a2e66cac92
yuriy.evseenko@gmail.com <yuriy.evseenko@gmail.com>
2019-03-29 19:40:23 +0200

    comments

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -69,6 +69,7 @@ export class HeroService {
   /** POST: add a new hero to the server */
   addHero(hero: Hero): Observable<Hero> {
     return this.http.post<Hero>(this.heroesUrl, hero, httpOptions).pipe(
+      // tslint:disable-next-line:no-shadowed-variable
       tap((hero: Hero) => this.log(`added hero w/ id=${hero.id}`)),
       catchError(this.handleError<Hero>('addHero'))
     );
